7f67986ec479abdaca94ed7024b44f3c0db88266
fred-corp <Frederic.druppel@icloud.com>
2024-09-30 17:34:54 +0200

    refactored code, removed debug code

--- waffle_platformio/src/main.cpp
+++ waffle_platformio/src/main.cpp
@@ -22,45 +22,13 @@ static bool ledState = false;
 static const UBaseType_t UITaskPriority = 2;
 static const UBaseType_t taskPriority = 1;
 
-static const char *TAG_MAIN = "MAIN";
-static const char *TAG_UI = "UI_Handler";
-static const char *TAG_TASK1 = "TASK1";
-static const char *TAG_TASK2 = "TASK2";
-static const char *TAG_RADIO = "RADIO";
+static const char *TAG_MAIN  = "[MAIN      ]";
+static const char *TAG_UI    = "[UI_Handler]";
+static const char *TAG_LED   = "[LED       ]";
+static const char *TAG_RADIO = "[RADIO     ]";
 
 UIHandler uiHandler(PIN_SDA, PIN_SCL);
 radioHandler sxradio(LORA_SCK, LORA_MISO, LORA_MOSI, LORA_SS, LORA_DIO0, LORA_DIO1, LORA_DIO2, LORA_RST);
-
-const char *model_info(esp_chip_model_t model) {
-    switch (model) {
-    case CHIP_ESP32:
-        return "ESP32";
-    case CHIP_ESP32S2:
-        return "ESP32S2";
-    case CHIP_ESP32S3:
-        return "ESP32S3";
-    case CHIP_ESP32C3:
-        return "ESP32C3";
-    case CHIP_ESP32H2:
-        return "ESP32H2";
-    case CHIP_ESP32C2:
-        return "ESP32C2";
-    default:
-        return "Unknown";
-    }
-}
-
-void print_chip_info() {
-    esp_chip_info_t chip_info;
-    esp_chip_info(&chip_info);
-    ESP_LOGI(TAG_TASK1, "Chip model %s with %d CPU core(s), WiFi%s%s, ",
-             model_info(chip_info.model), chip_info.cores,
-             (chip_info.features & CHIP_FEATURE_BT) ? "/BT" : "",
-             (chip_info.features & CHIP_FEATURE_BLE) ? "/BLE" : "");
-    unsigned major_rev = chip_info.revision / 100;
-    unsigned minor_rev = chip_info.revision % 100;
-    ESP_LOGI(TAG_TASK1, "silicon revision v%d.%d\n", major_rev, minor_rev);
-}
 
 void vUITask(void *pvParameters) {
     uiHandler.splashScreen();
@@ -88,22 +56,19 @@ void vUITask(void *pvParameters) {
     vTaskDelete(NULL);
 }
 
-void vTask1(void *pvParameters) {
-    for (;;) {
-        ESP_LOGI(TAG_TASK1, "Task1 is running");
-        ESP_LOGI(TAG_TASK1, "Task1 is running on core %d\n", xPortGetCoreID());
-        print_chip_info();
-        vTaskDelay(1000 / portTICK_PERIOD_MS);
-    }
-}
-
-void vTask2(void *pvParameters) {
+void vRadioTask(void *pvParameters) {
+    ESP_LOGI(TAG_RADIO, "Radio is running");
+    ESP_LOGI(TAG_RADIO, "Radio is running on core %d\n", xPortGetCoreID());
 
     sxradio.pocsagSendText(100, "TEST");
 
+    vTaskDelete(NULL);
+}
+
+void vLedTask(void *pvParameters) {
     for (;;) {
-        ESP_LOGI(TAG_TASK2, "Task2 is running");
-        ESP_LOGI(TAG_TASK2, "Task2 is running on core %d\n", xPortGetCoreID());
+        ESP_LOGI(TAG_LED, "LedTask is running");
+        ESP_LOGI(TAG_LED, "LedTask is running on core %d\n", xPortGetCoreID());
         ledState = !ledState;
         gpio_set_level(LED, ledState);
         vTaskDelay(500 / portTICK_PERIOD_MS);
@@ -116,17 +81,16 @@ void app_main() {
     sxradio.pocsagInit(frequency, offset, TAG_RADIO, ESP_LOG_INFO);
 
     esp_log_level_set(TAG_MAIN, ESP_LOG_WARN);
-    esp_log_level_set(TAG_TASK1, ESP_LOG_WARN);
-    esp_log_level_set(TAG_TASK2, ESP_LOG_WARN);
+    esp_log_level_set(TAG_LED, ESP_LOG_WARN);
 
-    xTaskCreate(vTask1,
-                "Task1",
+    xTaskCreate(vRadioTask,
+                "RadioTask",
                 5000,
                 NULL,
                 taskPriority,
                 NULL);
-    xTaskCreate(vTask2,         // Entry function of the task
-                "Task2",        // Name of the task
+    xTaskCreate(vLedTask,       // Entry function of the task
+                "LedTask",      // Name of the task
                 5000,           // The number of words to allocate for use as the task's
                                 // stack (arbitrary size enough for this task)
                 NULL,           // No parameter passed to the task
